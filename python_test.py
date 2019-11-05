80b3e79cd9175799677a0d2dc3c8145b327e1c6f
EETRAA <1024596314@qq.com>
2019-11-05 22:50:16 +0800

    Update python_test.py

--- python_test.py
+++ python_test.py
@@ -5,16 +5,16 @@ import os
 import requests
 
 def consume(q):
-    while(True):
+    while(q.qsize()!= 0):# changlog latest
         #name = threading.currentThread().getName()
         #print ()
         item = q.get()
         time.sleep(1)
         #time.sleep(3)  # spend 3 seconds to process or consume the tiem
         print ("get an item -->"+item+"-- current queue size -->"+str(q.qsize()))
-        proxies1 = {'http': "socks5://"+item}
+        proxies1 = {'https': "socks5://"+item}
         print (proxies1)
-        r=requests.get('http://www.baidu.com', proxies=proxies1, timeout=10)
+        r=requests.get('https://www.baidu.com', proxies=proxies1, timeout = 10)
         print(r.status_code)
         if r.status_code == 200:
             #of = open("C:\\Users\\hades\\Desktop\\o.txt","a")
@@ -30,12 +30,14 @@ def producer(q):
         #name = threading.currentThread().getName()
         #print ()
         #item = "item-" + str(i)
-        time.sleep(0.1)
+        print ("Now enter producer sub thread" + time.ctime())
+        # time.sleep(1)
         line = line.rstrip("\n")
         q.put(line)
         print ("put an item in queue"+str(q.qsize()))
         print ("******************"+str(q.queue))
-    q.join()
+        print (time.ctime())
+    # q.join()
  
 if __name__ == '__main__':
 	#of = open("C:\\Users\\hades\\Desktop\\temp_socks_list_parsed.txt")
@@ -45,12 +47,13 @@ if __name__ == '__main__':
     #1 thread to procuce
     t = threading.Thread(name = "ProducerThread", target=producer, args=(q,))
     t.start()
- 
+    time.sleep(5)
     threads_num = 3  # three threads to consume
     for i in range(threads_num):
         t = threading.Thread(name = "ConsumerThread-"+str(i), target=consume, args=(q,))
         t.start()
- 
+        # t.join() # block the consume threads
 
     q.join()
-    #of.close()
+    print("main thread ends")
+    of.close()
