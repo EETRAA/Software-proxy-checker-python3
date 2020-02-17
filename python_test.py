d8112a9a1786e3f6d690b144f6884b259349207e
EETRAA <1024596314@qq.com>
2020-02-17 22:19:51 +0800

    Update python_test.py

--- python_test.py
+++ python_test.py
@@ -9,24 +9,31 @@ def consume(q):
         #name = threading.currentThread().getName()
         #print ()
         item = q.get()
-        time.sleep(1)
+        #time.sleep(1)
         #time.sleep(3)  # spend 3 seconds to process or consume the tiem
         print ("get an item -->"+item+"-- current queue size -->"+str(q.qsize()))
         proxies1 = {'https': "socks5://"+item}
         print (proxies1)
-        r=requests.get('https://www.baidu.com', proxies=proxies1, timeout = 10)
-        print(r.status_code)
-        if r.status_code == 200:
-            #of = open("C:\\Users\\hades\\Desktop\\o.txt","a")
-            of.write(str(proxies1))
-            #
-    q.task_done()
- 
+        try:
+            r=requests.get('https://www.google.com', proxies=proxies1, timeout = 3)
+        except:
+            print("******************errs occurred******************")
+        
+        #print(r.status_code)
+        try:
+            if r.status_code == 200:
+                #of = open("C:\\Users\\hades\\Desktop\\o.txt","a")
+                of.write(str(proxies1))
+                #
+        except:
+            print("_______________errs occurred_______________")
+        q.task_done()
+
 		
 def producer(q):
     # the main thread will put new items to the queue
- 
-    for line in open("C:\\Users\\hades\\Desktop\\temp_socks_list_parsed.txt"):
+
+    for line in open("./temp_socks_list_parsed.txt"):
         #name = threading.currentThread().getName()
         #print ()
         #item = "item-" + str(i)
@@ -38,22 +45,44 @@ def producer(q):
         print ("******************"+str(q.queue))
         print (time.ctime())
     # q.join()
- 
+
 if __name__ == '__main__':
 	#of = open("C:\\Users\\hades\\Desktop\\temp_socks_list_parsed.txt")
-    of = open("C:\\Users\\hades\\Desktop\\o.txt","a")
-    q = queue.Queue(maxsize = 50)
-
+    of = open("./o.txt","a")
+    q = queue.Queue(maxsize = 2)
+    producer_i = 0
     #1 thread to procuce
     t = threading.Thread(name = "ProducerThread", target=producer, args=(q,))
     t.start()
-    time.sleep(5)
-    threads_num = 3  # three threads to consume
-    for i in range(threads_num):
-        t = threading.Thread(name = "ConsumerThread-"+str(i), target=consume, args=(q,))
-        t.start()
-        # t.join() # block the consume threads
+    #t.join()
+#    time.sleep(5)
+    threads_num = 10  # three threads to consume
+    #for i in range(threads_num):
+    t1 = threading.Thread(name = "ConsumerThread-1", target=consume, args=(q,))
+    t1.start()
+    t2 = threading.Thread(name = "ConsumerThread-2", target=consume, args=(q,))
+    t2.start()
+    t3 = threading.Thread(name = "ConsumerThread-3", target=consume, args=(q,))
+    t3.start()
+    t4 = threading.Thread(name = "ConsumerThread-4", target=consume, args=(q,))
+    t4.start()
+    t5 = threading.Thread(name = "ConsumerThread-5", target=consume, args=(q,))
+    t5.start()
+    t6 = threading.Thread(name = "ConsumerThread-6", target=consume, args=(q,))
+    t6.start()
+        
 
     q.join()
     print("main thread ends")
     of.close()
+    print("file o.txt successfully closed")
+
+
+
+#将数据存在内存里，完成之后再将数据转存到数据库
+#创建临时存储数据的文件夹
+#多线程消费与生产模型建立：抢食模型，慢食模型
+#threading的锁
+#！！创建类来写应用，哈哈哈哈
+#异常处理
+#变量现在是全局的，会判断失误，当上一个为通时，下一个会有概率出错
